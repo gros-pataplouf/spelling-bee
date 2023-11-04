7bcf36ed3ea236788f8d685a2a670ff22b4e33e3
gros-pataplouf <sceres@pm.me>
2023-11-04 21:47:31 +0100

    test: click on play button shows next screen

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,21 +1,47 @@
 
 import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
+import { fireEvent, getByText } from '@testing-library/react'
+
 import App from './App'
 
 describe('<App />', () => {
   test('App mounts properly', () => {
     const wrapper = render(<App />)
     expect(wrapper).toBeTruthy()
+  })
 
-    // Get by h1
+  test('There is a h1 containing the title "Spelling Bee"', () => {
+    const wrapper = render(<App />)
     const h1 = wrapper.container.querySelector('h1')
     expect(h1?.textContent).toBe('Spelling Bee')
+  })
 
-    // Get by text using the React testing library
-    const text = screen.getByText(
-      /How many words can you make with 7 letters?/i
-    );
+  test('Subtitle displayed correctly', () => {
+    const wrapper = render(<App />)
+    const text = screen.getByText("How many words can you make with 7 letters?");
     expect(text.textContent).toBeTruthy()
   })
+
+  // Get by text using the React testing library
+  test('Play button displayed', () => {
+    const wrapper = render(<App />)
+    const button = wrapper.container.querySelector('button')
+    expect(button?.textContent).toBe('Play')
+  })
+  it('Button disappears after clicking on it', () => {
+    const wrapper = render(<App />)
+    const button = wrapper.container.querySelector('button')
+    fireEvent(
+      getByText(button, 'Play'),
+      new MouseEvent('click', {
+        bubbles: true
+      }),
+    )
+    setTimeout(() => {
+      expect(button).toBeNull()
+    }, 100
+    )
+  })
+
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
+import {useState} from "react"
+
+
+
+
 function App() {
+
+
+  const [stateOfGame, setStateOfGame] = useState("welcome")
+
+  function showGame(e) {
+    console.log("hello")
+    e.preventDefault();
+    setStateOfGame("playing");
+        }
 
   return (
     <div className="bg-yellow-400 h-screen">
     <h1 className="font-semibold">Spelling Bee</h1>
-    <p>How many words can you make with 7 letters?</p>
-    <button className="btn btn-rounded text-white font-bold bg-black">Play</button>
-    </div>
+
+ {stateOfGame === "welcome" ?
+ <><p>How many words can you make with 7 letters?</p>
+    <button className="btn btn-rounded text-white font-bold bg-black" onClick={showGame}>Play</button>
+    </> : 
+    <div >Playing</div>
+  }    
+</div>
   )
 }
 
