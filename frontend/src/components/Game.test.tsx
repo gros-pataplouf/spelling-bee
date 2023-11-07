9ed5d8b181e2bfe5734536e85344be5b0512ffe3
gros-pataplouf <sceres@pm.me>
2023-11-07 20:19:40 +0100

    test: random shuffling letters except middle letter

--- frontend/src/components/Game.test.tsx
+++ frontend/src/components/Game.test.tsx
@@ -1,5 +1,5 @@
-import { describe, test, expect, it } from 'vitest'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { describe, test, expect } from 'vitest'
+import { render, fireEvent, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Game from './Game'
 
@@ -18,16 +18,6 @@ describe('<Game/>', () => {
         const game = render(<Game />)
         const textBoxes = game.container.querySelectorAll('#hive>svg>text')
         expect(textBoxes.length).toBe(7)
-    })
-    test('The text elements are rendered with svg text attributes', () => {
-        const game = render(<Game />)
-        const textBoxes = game.container.querySelectorAll('#hive>svg>text')
-        textBoxes.forEach(textBox => {
-            expect(textBox).toHaveAttribute("x")
-            expect(textBox).toHaveAttribute("y")
-            expect(textBox).toHaveAttribute("font-family")
-            expect(textBox).toHaveAttribute("font-size")
-        })
     })
     test('The text elements are rendered with svg text attributes', () => {
         const game = render(<Game />)
@@ -71,5 +61,20 @@ describe('<Game/>', () => {
         expect(screen.getByRole("input") as HTMLInputElement).toHaveAttribute("value",  "SOLI")
     })
 
+    test('There is a shuffle button', () => {
+        const game = render(<Game/>)
+        const shuffleButton = game.container.querySelector("#shuffle") as HTMLButtonElement
+        expect(shuffleButton).toBeInTheDocument()
+    })
 
+    test('Clicking the shuffle button will shuffle the letters, except for #middleLetter, which will stay the first Node', async () => {
+        const game = render(<Game/>)
+        const shuffleButton = game.container.querySelector("#shuffle") as HTMLButtonElement
+        expect(shuffleButton).toBeInTheDocument()
+        const letterArrayBefore = Array.from(game.container.querySelectorAll('#hive>svg>text')).map(node => node.textContent)
+        await userEvent.click(shuffleButton)
+        const letterArrayAfter = Array.from(game.container.querySelectorAll('#hive>svg>text')).map(node => node.textContent)
+        expect(letterArrayBefore[0]).toEqual(letterArrayAfter[0])
+        expect(letterArrayBefore.slice(1,7)).not.toEqual(letterArrayAfter.slice(1,7))
+    })
 })
--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Cell from './Cell'
 import { BaseSyntheticEvent } from 'react'
 const strg = "iloqstu"
 const letterArray = Array.from(strg.toUpperCase())
-console.log(letterArray)
 
 function Game() {
     const [input, setInput] = useState([""])
+    const [letters, setLetters ] = useState(letterArray)
+
     function handleChange(e: BaseSyntheticEvent){
         const inputEvent = e.nativeEvent as InputEvent
         if (inputEvent.data && strg.includes(inputEvent.data.toLowerCase())) {
@@ -15,13 +16,24 @@ function Game() {
             setInput(input.slice(0, input.length-1))
         }
     }
+    function shuffle() {
+        const otherLettersOld = letters.slice(1,7)
+        const otherLettersNew:string[] = []
+        while (otherLettersOld.length) {
+            const [removedLetter] = otherLettersOld.splice(Math.floor(Math.random()*otherLettersOld.length),1) 
+            otherLettersNew.push(removedLetter)
+        }
+        setLetters([letterArray[0], ...otherLettersNew])
+    }
 
     return (
     <div>
     <input id="input" role="input" placeholder="Type or click" onChange={handleChange} value={input.join("")}/>
     <div id="hive">
-        {letterArray.map(letter => <Cell letter={letter} middleLetter={letter === strg[0].toUpperCase()} key={letter} input={input} setInput={setInput}/>)}
+        {letters.map(letter => <Cell letter={letter} middleLetter={letter === strg[0].toUpperCase()} key={letter} input={input} setInput={setInput}/>)}
     </div>
+    <button id="shuffle" onClick={shuffle}>Shuffle</button>
+
     </div>)
 }
 
