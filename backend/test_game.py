d8b442a48c129f9b9532f4a21d49e9ae7f21ef65
gros-pataplouf <sceres@pm.me>
2024-04-01 21:41:06 +0200

    test: socket query endpoint works

--- backend/test_game.py
+++ backend/test_game.py
@@ -8,7 +8,6 @@ def is_valid_uuid(input):
     uuid_regex = re.compile("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
     return uuid_regex.match(input)
 
-
 @pytest.fixture
 def player():
     return Player("Plouf", uuid4())
--- backend/test_socket.py
+++ backend/test_socket.py
@@ -1,6 +1,7 @@
 
 import pytest, os, django
 from game.consumers import GameConsumer, QueryConsumer
+from game.game import Player, Game
 from uuid import uuid4
 from channels.testing import WebsocketCommunicator
 
@@ -8,10 +9,26 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
 
 django.setup()
 
+@pytest.fixture
+def player():
+    return Player("Plouf", uuid4())
+
+@pytest.fixture
+def games():
+    return [Game(timeout=2)]
+
+
 
 @pytest.mark.asyncio
 async def test_game_connection_can_be_established():
     communicator = WebsocketCommunicator(GameConsumer.as_asgi(), f"ws://localhost/{uuid4()}")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    await communicator.disconnect()
+
+@pytest.mark.asyncio
+async def test_query_connection_can_be_established():
+    communicator = WebsocketCommunicator(QueryConsumer.as_asgi(), "ws://localhost/query")
     connected, subprotocol = await communicator.connect()
     assert connected
     await communicator.disconnect()
