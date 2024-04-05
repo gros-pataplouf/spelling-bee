11079437712f76dcf7e92aebfb78777c9bb399f9
gros-pataplouf <sceres@pm.me>
2024-04-05 22:02:27 +0200

    refactor

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -6,8 +6,24 @@ from .game import Game, Player
 games = []
 user_groups = {}
 
+ERROR_MESSAGES = {
+    "not_ex": "game does not exist", 
+    'full': "game already full"
+}
 
 class QueryConsumer(AsyncWebsocketConsumer):
+    def generate_error_message(self, text):
+        template = {
+                    "type": "game_info",
+                    "message": {
+                        "phaseOfGame": "error",
+                        "message": {"category": "result", "content": text, "points": None}}}
+        return template
+    def generate_status_message(self, game, status=None):
+        template = {"type": "game_info", "message": {"phaseOfGame": status or game.status, "multiPlayer": game.multiplayer}}
+        return template
+
+
     async def connect(self):
         self.temp_user_group = None
         await self.accept()
@@ -17,33 +33,19 @@ class QueryConsumer(AsyncWebsocketConsumer):
         filtered_games = list(filter(lambda game: game.uuid == requested_game_id, games))
         self.temp_user_group = "group_" + str(uuid4())
         await self.channel_layer.group_add(self.temp_user_group, self.channel_name)
-        if filtered_games:
-            print("we have", message.get("player1Id"), [p.uuid for p in filtered_games[0].players])
         if not filtered_games:
             await self.channel_layer.group_send(
-                self.temp_user_group, {
-                    "type": "game_info",
-                    "message": {
-                        "phaseOfGame": "error",
-                        "message": {"category": "result", "content": "game does not exist", "points": None}}})
+                self.temp_user_group, self.generate_error_message(ERROR_MESSAGES["not_ex"]))
         elif filtered_games and not filtered_games[0].multiplayer and message.get("player1Id") != filtered_games[0].players[0].uuid:
             await self.channel_layer.group_send(
-                self.temp_user_group, {
-                    "type": "game_info",
-                    "message": {
-                        "phaseOfGame": "error",
-                        "message": {"category": "result", "content": "game already full", "points": None}}})
+                self.temp_user_group, self.generate_error_message(ERROR_MESSAGES["full"]))
         elif filtered_games and len(filtered_games[0].players) > 1 and message.get("player1Id") not in [p.uuid for p in filtered_games[0].players]:
             await self.channel_layer.group_send(
-                self.temp_user_group, {
-                    "type": "game_info",
-                    "message": {
-                        "phaseOfGame": "error",
-                        "message": {"category": "result", "content": "game already full", "points": None}}})
+                 self.temp_user_group, self.generate_error_message(ERROR_MESSAGES["full"]))
         elif filtered_games and filtered_games[0].multiplayer and message.get("player1Id") not in [p.uuid for p in filtered_games[0].players]:
-            await self.channel_layer.group_send(self.temp_user_group, {"type": "game_info", "message": {"phaseOfGame": "joining", "multiPlayer": filtered_games[0].multiplayer}})
+            await self.channel_layer.group_send(self.temp_user_group, self.generate_status_message(filtered_games[0], status="joining"))
         elif filtered_games:
-            await self.channel_layer.group_send(self.temp_user_group, {"type": "game_info", "message": {"phaseOfGame": filtered_games[0].status, "multiPlayer": filtered_games[0].multiplayer}})
+            await self.channel_layer.group_send(self.temp_user_group, self.generate_status_message(filtered_games[0]))
 
         
     async def disconnect(self, close_code):
