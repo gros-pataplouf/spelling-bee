169a4682e3f2f560dfaed41cf917152cc155ccae
gros-pataplouf <sceres@pm.me>
2024-04-07 19:22:54 +0200

    refactor: game mixin

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -11,8 +11,40 @@ ERROR_MESSAGES = {
     'full': "game already full"
 }
 
-class GameConsumer(AsyncWebsocketConsumer):
+class GameMixin:
+    """
+    methods shared by Gameconsumer and QueryConsumer
+    """
+    def translate_game_object(self, game: Game, player_id):
+        def get_player(id, opponent=False) -> Player:
+            if not opponent:
+                filtered_players = list(filter(lambda p: p.uuid == id, game.players))
+                return filtered_players[0] if filtered_players else None
+            else:
+                filtered_players = list(filter(lambda p: p.uuid != id, game.players))
+                return filtered_players[0] if filtered_players else None
+        player1 = get_player(player_id)
+        player2 = get_player(player_id, opponent=True)
 
+        return {
+                "gameId": game.uuid,
+                "gameTimeStamp": game.timeout,
+                "letters": game.letterset,
+                "phaseOfGame": game.status,
+                "player1Id": player1.uuid,
+                "player1Name": player1.name,
+                "player1GuessedWords": player1.guessed_words,
+                "player1Points": player1.points,
+                "multiPlayer": game.multiplayer,
+                "player2Id": player2.uuid if player2 else None,
+                "player2Name": player2.name if player2 else None,
+                "player2GuessedWords": player2.guessed_words if player2 else None,
+                "player2Points": player2.points if player2 else None
+            }
+
+
+
+class GameConsumer(AsyncWebsocketConsumer, GameMixin):
 
     async def connect(self):
         self.game_uuid = self.scope["path"].strip("/")
@@ -75,39 +107,12 @@ class GameConsumer(AsyncWebsocketConsumer):
         print(feedback)
         await self.send(text_data=feedback)
 
-    @classmethod
-    def translate_game_object(self, game: Game, player_id):
-        def get_player(id, opponent=False) -> Player:
-            if not opponent:
-                filtered_players = list(filter(lambda p: p.uuid == id, game.players))
-                return filtered_players[0] if filtered_players else None
-            else:
-                filtered_players = list(filter(lambda p: p.uuid != id, game.players))
-                return filtered_players[0] if filtered_players else None
-        player1 = get_player(player_id)
-        player2 = get_player(player_id, opponent=True)
-
-        return {
-                "gameId": game.uuid,
-                "gameTimeStamp": game.timeout,
-                "letters": game.letterset,
-                "phaseOfGame": game.status,
-                "player1Id": player1.uuid,
-                "player1Name": player1.name,
-                "player1GuessedWords": player1.guessed_words,
-                "player1Points": player1.points,
-                "multiPlayer": game.multiplayer,
-                "player2Id": player2.uuid if player2 else None,
-                "player2Name": player2.name if player2 else None,
-                "player2GuessedWords": player2.guessed_words if player2 else None,
-                "player2Points": player2.points if player2 else None
-            }
 
 
 
 
 
-class QueryConsumer(AsyncWebsocketConsumer):
+class QueryConsumer(AsyncWebsocketConsumer, GameMixin):
 
     def generate_error_message(self, text):
         template = {
@@ -159,7 +164,7 @@ class QueryConsumer(AsyncWebsocketConsumer):
         print(event)
         game = event["game"]
         id = event["id"]
-        feedback = json.dumps(GameConsumer.translate_game_object(game, player_id=id))
+        feedback = json.dumps(self.translate_game_object(game, player_id=id))
         await self.send(text_data=feedback)
 
         
